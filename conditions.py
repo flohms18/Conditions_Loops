5c65e0ccbbe6ec48785b934d9f9bf8938e22365c
flotr18 <trouilletflorent@outlook.com>
2025-02-06 10:28:50 +0100

    Add Pos Function and greatest

--- conditions.py
+++ conditions.py
@@ -6,5 +6,20 @@ def OddOrEven(x):
 
 OddOrEven(4)
 
+def IsPositive(x):
+    if x > 0:
+        print("x is positive !")
+    else:
+        print("x is negative !")
 
+IsPositive(-4)
 
+def GreatestNumber(x,y,z):
+    if x > y and x > z:
+        print("x is the greatest number !")
+    elif y > x and y > z:
+        print("y is the greatest number !")
+    else:
+        print("z is the greatest number !")
+
+GreatestNumber(4,5,6)
