e63f612b34472f061e781ba466877a295489b28c
flotr18 <trouilletflorent@outlook.com>
2025-02-06 10:39:52 +0100

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -45,3 +45,11 @@ def IsPrimeNumber(x):
 
 IsPrimeNumber(7)
 
+def Operator(x,y):
+    print("Addition: ", x + y)
+    print("Subtraction: ", x - y)
+    print("Multiplication: ", x * y)
+    print("Division: ", x / y)
+
+Operator(4,2)
+
